ee2d18569341301af8a571cb088b8b7523df2573
gogodick <geoffrey_lv@163.com>
2017-06-13 16:06:33 +0800

    Update crawler.sh

--- crawler.sh
+++ crawler.sh
@@ -1,3 +1,16 @@
 URL="https://sale.jd.com/act/XUh2CDEPdI6YuzZf.html"
+HOUR=0
+FILE="crawler.txt"
+if [ -e $FILE ]; then
+    rm $FILE
+fi
 echo "Login to JD ..."
-python py/jd_crawler.py -u $URL -l "crawler.txt"
+python py/jd_crawler.py -u $URL -l $FILE
+#python py/jd_crawler.py -u $URL -hh $HOUR -l $FILE
+USER="jd_coupon_log"
+PASS="jd123456"
+TO="jd_coupon_log@163.com"
+if [ -e $FILE ]; then
+    echo "Send email to $TO ..."
+    python py/email163.py -u $USER -p $PASS -t $TO -f $FILE
+fi
