bfe404ab474d76826634c19665a981ad06bde5bb
gogodick <geoffrey_lv@163.com>
2017-06-15 14:12:29 +0800

    Add process number

--- crawler.sh
+++ crawler.sh
@@ -1,12 +1,13 @@
 URL="https://sale.jd.com/act/XUh2CDEPdI6YuzZf.html"
 HOUR=0
+PROCESS=8
 FILE="crawler.txt"
 if [ -e $FILE ]; then
     rm $FILE
 fi
 echo "Login to JD ..."
-python py/jd_crawler.py -u $URL -l $FILE
-#python py/jd_crawler.py -u $URL -hh $HOUR -l $FILE
+python py/jd_crawler.py -u $URL -l $FILE -p $PROCESS
+#python py/jd_crawler.py -u $URL -hh $HOUR -p $PROCESS -l $FILE
 USER="jd_coupon_log"
 PASS="jd123456"
 TO="jd_coupon_log@163.com"
