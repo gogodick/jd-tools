1b398d5dd7a8359fb0712f527c5fb506429e2bb7
gogodick <geoffrey_lv@163.com>
2017-06-19 14:48:44 +0800

    Update crawler.sh

--- crawler.sh
+++ crawler.sh
@@ -1,17 +1,23 @@
 URL="https://sale.jd.com/act/XUh2CDEPdI6YuzZf.html"
-HOUR=0
-PROCESS=8
-FILE="crawler.txt"
-if [ -e $FILE ]; then
-    rm $FILE
+DEPTH=2
+PROCESS=16
+LOG_FILE="log_crawler.txt"
+COUPON_FILE="my_coupon.csv"
+LOTTERY_FILE="my_lottery.csv"
+if [ -e $LOG_FILE ]; then
+    rm $LOG_FILE
 fi
-echo "Login to JD ..."
-python py/jd_crawler.py -u $URL -l $FILE -p $PROCESS
-#python py/jd_crawler.py -u $URL -hh $HOUR -p $PROCESS -l $FILE
+if [ -e $COUPON_FILE ]; then
+    rm $COUPON_FILE
+fi
+if [ -e $LOTTERY_FILE ]; then
+    rm $LOTTERY_FILE
+fi
+python py/jd_crawler.py -p $PROCESS -d $DEPTH -cf $COUPON_FILE -lf $LOTTERY_FILE -l $LOG_FILE
 USER="jd_coupon_log"
 PASS="jd123456"
 TO="jd_coupon_log@163.com"
 if [ -e $FILE ]; then
     echo "Send email to $TO ..."
-    python py/email163.py -u $USER -p $PASS -t $TO -f $FILE
+    python py/email163.py -u $USER -p $PASS -t $TO -f $LOG_FILE
 fi
