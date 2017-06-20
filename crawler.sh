723d8deef8849de5a03b57954d6d6e1883520b74
gogodick <geoffrey_lv@163.com>
2017-06-20 11:27:00 +0800

    Update crawler.sh

--- crawler.sh
+++ crawler.sh
@@ -1,5 +1,4 @@
-URL="https://sale.jd.com/act/XUh2CDEPdI6YuzZf.html"
-DEPTH=2
+DEPTH=5
 PROCESS=16
 LOG_FILE="log_crawler.txt"
 COUPON_FILE="my_coupon.csv"
@@ -13,7 +12,8 @@ fi
 if [ -e $LOTTERY_FILE ]; then
     rm $LOTTERY_FILE
 fi
-python py/jd_crawler.py -p $PROCESS -d $DEPTH -cf $COUPON_FILE -lf $LOTTERY_FILE -l $LOG_FILE
+python py/jd_crawler.py -p $PROCESS -d $DEPTH -cf $COUPON_FILE -lf $LOTTERY_FILE
+python py/jd_lottery.py -f $LOTTERY_FILE -l $LOG_FILE
 USER="jd_coupon_log"
 PASS="jd123456"
 TO="jd_coupon_log@163.com"
