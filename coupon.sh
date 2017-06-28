58e3bcf1419392e2f2856be99886ddfcdacced66
gogodick <geoffrey_lv@163.com>
2017-06-28 11:01:35 +0800

    Update coupon.sh

--- coupon.sh
+++ coupon.sh
@@ -1,6 +1,6 @@
-#URL="https://coupon.jd.com/ilink/couponSendFront/send_index.action?key=5295463f6ce84ae8afce9624093e2fab&roleId=6932641&to=sale.jd.com/act/bgx0ywve5awmvnxp.html&"
-URL="http://coupon.jd.com/ilink/couponSendFront/send_index.action?key=cf5ca79d97fa451b9554df5229d711e4&roleId=6965414&to=sale.jd.com/act/qts6qbhnoml0kj1b.html&"
-HOUR=20
+KEY="d2b40098dff44d7993830cc672b66d06"
+ROLEID="7190126"
+HOUR=12
 MINUTE=00
 PROCESS=32
 FILE="coupon.txt"
@@ -8,7 +8,7 @@ if [ -e $FILE ]; then
     rm $FILE
 fi
 echo "Login to JD ..."
-python py/jd_coupon.py -u $URL -hh $HOUR -m $MINUTE -p $PROCESS -l $FILE
+python py/jd_coupon.py -k $KEY -r $ROLEID -hh $HOUR -m $MINUTE -p $PROCESS -l $FILE
 USER="jd_coupon_log"
 PASS="jd123456"
 TO="jd_coupon_log@163.com"
