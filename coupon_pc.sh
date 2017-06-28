b41928d37e8e8e30e531956b9d4c4218059783d6
gogodick <geoffrey_lv@163.com>
2017-06-28 12:47:28 +0800

    New shell script to run PC

---
+++ coupon_pc.sh
@@ -0,0 +1,18 @@
+KEY="374b5335e6d14ab88f87061ef41f7330"
+ROLEID="7140368"
+HOUR=12
+MINUTE=46
+PROCESS=32
+FILE="coupon.txt"
+if [ -e $FILE ]; then
+    rm $FILE
+fi
+echo "Login to JD ..."
+python py/jd_coupon_pc.py -k $KEY -r $ROLEID -hh $HOUR -m $MINUTE -p $PROCESS -l $FILE
+USER="jd_coupon_log"
+PASS="jd123456"
+TO="jd_coupon_log@163.com"
+if [ -e $FILE ]; then
+    echo "Send email to $TO ..."
+    python py/email163.py -u $USER -p $PASS -t $TO -f $FILE
+fi
