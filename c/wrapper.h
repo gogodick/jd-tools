0487e2759eb629be4548e52950df0db9795d9e5f
gogodick <geoffrey_lv@163.com>
2017-07-04 17:05:21 +0800

    Update wrapper.h

--- c/wrapper.h
+++ c/wrapper.h
@@ -7,11 +7,12 @@ struct MemoryStruct {
 
 void print_cookies(CURL *curl);
 void print_string(char *input, int length);
-int find_string(char *input, char *start, char *end, int *pos, int *length);
+int find_string_start(char *input, char *start, char *end, int *pos, int *length);
+int find_string_end(char *input, char *start, char *end, int *pos, int *length);
 int jd_setup(CURL *curl, char *filename);
 int jd_get(CURL *curl, struct MemoryStruct *chunk_ptr, char *url, char *referer);
+int jd_post(CURL *curl, struct MemoryStruct *chunk_ptr, char *url, char *data);
 double get_network_time(CURL *curl);
 void set_local_time(CURL *curl);
 double get_local_time();
-
 
