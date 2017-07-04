3d147cade1b8e680f94e8e646ee817db60a68927
gogodick <geoffrey_lv@163.com>
2017-07-04 17:03:56 +0800

    Create coupon.sh

---
+++ coupon.sh
@@ -0,0 +1,24 @@
+KEY="92cdc857ef844b5ba07f2496a998c341"
+ROLEID="7290231"
+HOUR=20
+MINUTE=00
+PROCESS=32
+COOKIEFILE="mobile_cookie.dat"
+FILE="coupon.log"
+TARGET="c/coupon"
+ROOT=`PWD`
+cd c
+make clean && make
+cd $ROOT
+if [ ! -e $TARGET ]; then
+    cd c
+    make
+    cd $ROOT
+fi
+if [ ! -e $TARGET ]; then
+    return
+fi
+if [ -e $FILE ]; then
+    rm $FILE
+fi
+$TARGET "mobile_cookie.dat" $KEY $ROLEID
