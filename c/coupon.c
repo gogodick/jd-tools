1b995c1cdfc6006105eef123e960cc4039e15686
gogodick <geoffrey_lv@163.com>
2017-07-04 17:04:51 +0800

    Update coupon.c

--- c/coupon.c
+++ c/coupon.c
@@ -6,16 +6,60 @@
 #include <sys/time.h>
 #include "wrapper.h"
 
+int coupon_click(CURL *curl)
+{
+    struct MemoryStruct chunk;
+    int ret = 0;
 
+    chunk.memory = malloc(1);
+    chunk.size = 0;
+    ret = jd_post(curl, &chunk, "http://coupon.m.jd.com/coupons/submit.json", NULL);
+    if (ret != 0) {
+        goto ERROR_EXIT;
+    }
+    print_string(chunk.memory, chunk.size);
+ERROR_EXIT:
+    if (NULL != chunk.memory) {
+        free(chunk.memory);
+        chunk.memory = NULL;
+    }
+    return ret;
+}
+
+int coupon_dig(CURL *curl, char *key, char *role_id)
+{
+    char payload[256] = {0};
+    int pos = 0, length = 0;
+    struct MemoryStruct chunk;
+    int ret = 0;
+
+    chunk.memory = malloc(1);
+    chunk.size = 0;
+    strcat(payload, "key=");
+    strcat(payload, key);
+    strcat(payload, "&roleId=");
+    strcat(payload, role_id);
+    ret = jd_post(curl, &chunk, "http://coupon.m.jd.com/coupons/show.action", payload);
+    if (ret != 0) {
+        goto ERROR_EXIT;
+    }
+    //print_string(chunk.memory, chunk.size);
+    ret = find_string_end(chunk.memory, "value=\"", "\" name=\"couponKey\"", &pos, &length);
+ERROR_EXIT:
+    if (NULL != chunk.memory) {
+        free(chunk.memory);
+        chunk.memory = NULL;
+    }
+    return ret;
+}
 
 int main(int argc, char* argv[]) 
 {
     CURL *curl;
     int ret = 0;
-    struct MemoryStruct chunk;
 
-    if (argc < 2) {
-        printf("Please specify cookie file...\n");
+    if (argc < 4) {
+        printf("Please specify cookie file, key and role_id\n");
         exit(1);
     }
     curl_global_init(CURL_GLOBAL_ALL);
@@ -26,28 +70,19 @@ int main(int argc, char* argv[])
             ret = 1;
             goto ERROR_EXIT;
         }
-        chunk.memory = malloc(1);
-        chunk.size = 0;
+        ret = coupon_dig(curl, argv[2], argv[3]);
+        if (ret != 0) {
+            ret = 1;
+            goto ERROR_EXIT;
+        }
         set_local_time(curl);
-        set_local_time(curl);
-        set_local_time(curl);
-        set_local_time(curl);
-        set_local_time(curl);
-        set_local_time(curl);
-        set_local_time(curl);
-        set_local_time(curl);
-        set_local_time(curl);
-        set_local_time(curl);
+        coupon_click(curl);
     } else {
         fprintf(stderr, "Curl init failed!\n");
         ret = 1;
         goto ERROR_EXIT;
     }
 ERROR_EXIT:
-    if (NULL != chunk.memory) {
-        free(chunk.memory);
-        chunk.memory = NULL;
-    }
     curl_global_cleanup();
     return ret;
 }
