8e57c0653ae5f7b81733b239513aab31f40e335c
gogodick <geoffrey_lv@163.com>
2017-07-05 10:34:26 +0800

    Update wrapper.h

--- c/wrapper.h
+++ c/wrapper.h
@@ -12,7 +12,7 @@ int find_string_end(char *input, char *start, char *end, int *pos, int *length);
 int jd_setup(CURL *curl, char *filename);
 int jd_get(CURL *curl, struct MemoryStruct *chunk_ptr, char *url, char *referer);
 int jd_post(CURL *curl, struct MemoryStruct *chunk_ptr, char *url, char *data);
-int jd_post_fast(CURL *curl, struct MemoryStruct *chunk_ptr, char *url, char *data);
+int jd_post_fast(CURL *curl, struct MemoryStruct *chunk_ptr, char *url, char *data, int num);
 double get_network_time(CURL *curl);
 void set_local_time(CURL *curl);
 double get_local_time();
