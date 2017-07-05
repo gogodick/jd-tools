a8ce0a129f0e1af83f237587da14ee7f49f06d41
gogodick <geoffrey_lv@163.com>
2017-07-05 09:11:37 +0800

    Update coupon.c

--- c/coupon.c
+++ c/coupon.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -7,6 +8,10 @@
 #include "wrapper.h"
 
 #define COUPON_PAYLOAD_SIZE 1024
+double relax_limit = 60.0;
+double busy_limit = 0.5;
+int duration = 5;
+char *coupon_url = "http://coupon.m.jd.com/coupons/submit.json";
 char coupon_payload[COUPON_PAYLOAD_SIZE] = {0};
 
 int coupon_click(CURL *curl)
@@ -16,7 +21,7 @@ int coupon_click(CURL *curl)
 
     chunk.memory = malloc(1);
     chunk.size = 0;
-    ret = jd_post(curl, &chunk, "http://coupon.m.jd.com/coupons/submit.json", coupon_payload);
+    ret = jd_post(curl, &chunk, coupon_url, coupon_payload);
     if (ret != 0) {
         goto ERROR_EXIT;
     }
@@ -31,7 +36,7 @@ ERROR_EXIT:
 
 int coupon_click_fast(CURL *curl)
 {
-    return jd_post_fast(curl, NULL, "http://coupon.m.jd.com/coupons/submit.json", coupon_payload);
+    return jd_post_fast(curl, NULL, coupon_url, coupon_payload);
 }
 
 int coupon_dig(CURL *curl, char *key, char *role_id)
@@ -91,10 +96,40 @@ ERROR_EXIT:
     return ret;
 }
 
+void coupon_relax_wait(CURL *curl, double target, int delay)
+{
+    double diff = 0;
+    set_local_time(curl);
+    while (1) {
+        coupon_click(curl);
+        diff = compare_local_time(target);
+        if ((diff < relax_limit) && (diff > -relax_limit)) {
+            break;
+        }
+        sleep(delay);
+    }
+    return;
+}
+
+void coupon_busy_wait(CURL *curl, double target)
+{
+    double diff = 0;
+    set_local_time(curl);
+    while (1) {
+        diff = compare_local_time(target);
+        if (diff < busy_limit) {
+            break;
+        }
+    }
+    return;
+}
+
 int main(int argc, char* argv[]) 
 {
     CURL *curl;
     int hour = 0, minute = 0, process = 0;
+    int h, m, s;
+    double target = 0;
     int ret = 0;
 
     if (argc < 7) {
@@ -104,7 +139,7 @@ int main(int argc, char* argv[])
     hour = atoi(argv[4]);
     minute = atoi(argv[5]);
     process = atoi(argv[6]);
-    printf("main: %d, %d, %d\n", hour, minute, process);
+    target = hour*3600+minute;
     curl_global_init(CURL_GLOBAL_ALL);
     curl = curl_easy_init();
     if (curl) {
@@ -118,11 +153,15 @@ int main(int argc, char* argv[])
             ret = 1;
             goto ERROR_EXIT;
         }
-        set_local_time(curl);
-        coupon_click_fast(curl);
-        coupon_click_fast(curl);
-        coupon_click_fast(curl);
-        coupon_click(curl);
+        coupon_relax_wait(curl, target, 5);
+        format_local_time(&h, &m, &s);
+        printf("##########################################\n");
+        printf("Start time %02d:%02d:%02d\n", h, m, s);
+        coupon_busy_wait(curl, target);
+        sleep(duration);
+        format_local_time(&h, &m, &s);
+        printf("##########################################\n");
+        printf("Stop time %02d:%02d:%02d\n", h, m, s);
     } else {
         fprintf(stderr, "Curl init failed!\n");
         ret = 1;
