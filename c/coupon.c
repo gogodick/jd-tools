5da1b4feb29a10c115ce90fc89d337ee54a02270
gogodick <geoffrey_lv@163.com>
2017-07-05 10:35:10 +0800

    Update coupon.c

--- c/coupon.c
+++ c/coupon.c
@@ -4,15 +4,39 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <time.h>
+#include <pthread.h>
 #include <sys/time.h>
 #include "wrapper.h"
 
+struct task_params
+{
+    int id;
+    char *file;
+};
+
+#define MAX_TASK 32
 #define COUPON_PAYLOAD_SIZE 1024
 double relax_limit = 60.0;
 double busy_limit = 0.5;
 int duration = 5;
 char *coupon_url = "http://coupon.m.jd.com/coupons/submit.json";
 char coupon_payload[COUPON_PAYLOAD_SIZE] = {0};
+int wait_flag = 0;
+int run_flag = 0;
+int cnt_array[MAX_TASK] = {0};
+CURLSH* share_handle = NULL;;
+
+void set_share_handle(CURL* curl_handle)
+{
+    if (!share_handle)
+    {
+        share_handle = curl_share_init();
+	curl_share_setopt(share_handle, CURLSHOPT_SHARE, CURL_LOCK_DATA_DNS);
+    }
+    curl_easy_setopt(curl_handle, CURLOPT_SHARE, share_handle);
+    curl_easy_setopt(curl_handle, CURLOPT_DNS_CACHE_TIMEOUT, 60 * 5);
+    return;
+}
 
 int coupon_click(CURL *curl)
 {
@@ -34,9 +58,9 @@ ERROR_EXIT:
     return ret;
 }
 
-int coupon_click_fast(CURL *curl)
+int coupon_click_fast(CURL *curl, int num)
 {
-    return jd_post_fast(curl, NULL, coupon_url, coupon_payload);
+    return jd_post_fast(curl, NULL, coupon_url, coupon_payload, num);
 }
 
 int coupon_dig(CURL *curl, char *key, char *role_id)
@@ -124,12 +148,36 @@ void coupon_busy_wait(CURL *curl, double target)
     return;
 }
 
+void *coupon_click_task(void *arg)
+{
+    int cnt = 0;
+    CURL *curl;
+    struct task_params *params = arg;
+
+    printf("Task %d start...\n", params->id+1);
+    curl_global_init(CURL_GLOBAL_ALL);
+    curl = curl_easy_init();
+    set_share_handle(curl);
+    curl_easy_setopt(curl, CURLOPT_COOKIEFILE, params->file);
+    while(wait_flag != 0);
+    while(run_flag != 0) {
+        cnt += coupon_click_fast(curl, 8);
+    }
+    coupon_click(curl);
+    curl_easy_cleanup(curl);
+    cnt_array[params->id] = cnt;
+    return NULL;
+}
+
 int main(int argc, char* argv[]) 
 {
     CURL *curl;
     int hour = 0, minute = 0, process = 0;
     int h, m, s;
+    int i = 0, cnt = 0;
+    pthread_t tid[MAX_TASK];
     double target = 0;
+    struct task_params params[MAX_TASK];
     int ret = 0;
 
     if (argc < 7) {
@@ -139,9 +187,13 @@ int main(int argc, char* argv[])
     hour = atoi(argv[4]);
     minute = atoi(argv[5]);
     process = atoi(argv[6]);
+    if (process > MAX_TASK) {
+        process = MAX_TASK;
+    }
     target = hour*3600+minute;
     curl_global_init(CURL_GLOBAL_ALL);
     curl = curl_easy_init();
+    set_share_handle(curl);
     if (curl) {
         ret = jd_setup(curl, argv[1]);
         if (ret != 0) {
@@ -153,15 +205,39 @@ int main(int argc, char* argv[])
             ret = 1;
             goto ERROR_EXIT;
         }
+        wait_flag = 1;
+        run_flag = 1;
         coupon_relax_wait(curl, target, 5);
         format_local_time(&h, &m, &s);
         printf("##########################################\n");
         printf("Start time %02d:%02d:%02d\n", h, m, s);
+        for (i = 0; i < process; i++) {
+            params[i].id = i;
+            params[i].file = argv[1];
+            ret = pthread_create(&tid[i], NULL, coupon_click_task, (void *)&params[i]);
+            if (ret) {
+                fprintf(stderr, "Create task %d failed!\n", i+1);
+                goto ERROR_EXIT;
+            }
+        }
         coupon_busy_wait(curl, target);
+        wait_flag = 0;
         sleep(duration);
         format_local_time(&h, &m, &s);
         printf("##########################################\n");
         printf("Stop time %02d:%02d:%02d\n", h, m, s);
+        run_flag = 0;
+        for (i = 0; i < process; i++) {
+            ret = pthread_join(tid[i], NULL);
+            if (ret) {
+                fprintf(stderr, "Wait task %d failed!\n", i+1);
+                goto ERROR_EXIT;
+            }
+        }
+        for (i = 0; i < process; i++) {
+            cnt += cnt_array[i];
+        }
+        printf("Click %d times in %d seconds\n", cnt, duration);
     } else {
         fprintf(stderr, "Curl init failed!\n");
         ret = 1;
