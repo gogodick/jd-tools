8c43b2a50f7b0a1bbcdbc9ac1f260d012b0347f2
gogodick <geoffrey_lv@163.com>
2017-10-12 14:23:00 +0800

    Update jd_coupon_mobile.py

--- py/jd_coupon_mobile.py
+++ py/jd_coupon_mobile.py
@@ -95,7 +95,7 @@ class JDCoupon(JDWrapper):
     def click_fast(self, count):
         try:
             url = 'http://coupon.m.jd.com/coupons/submit.json'
-            return [self.sess.post(url, data = self.coupon_data, timeout=5) for i in range(count)]
+            return [self.sess.post(url, data = self.coupon_data, verify=False) for i in range(count)]
         except Exception, e:
             return []
 
