7c7a7ff01448c43bce8f764bf31be349151ddcf0
gogodick <geoffrey_lv@163.com>
2017-11-03 07:05:30 +0800

    update

--- coupon_mobile.sh
+++ coupon_mobile.sh
@@ -1,7 +1,8 @@
 URL=''
-KEY="450ccbe232a94f3cbdc852978048f213"
-ROLEID="8839369"
-HOUR=20
+URL='http://api.m.jd.com/client.action?functionId=newBabelAwardCollection&clientVersion=6.5.2&build=53662&client=android&androidId=e6f3d6adfea10bdd&installtionId=cd2474c771884269a103c12652b948d9&sdkVersion=23&uuid=357755070126840-e458b88e8e4c&st=1509660876593&sign=11e7dddb8bdb03fb43ec9ef95437dbe8&sv=100&body=%7B%22activityId%22%3A%22o1SAPkr8McqBZ8N1N9LhevAGKQg%22%2C%22scene%22%3A%221%22%2C%22args%22%3A%22key%3Debbf381a37944e3f92ede837445d7172%2CroleId%3D8754258%22%7D'
+KEY="ebbf381a37944e3f92ede837445d7172"
+ROLEID="8754258"
+HOUR=08
 MINUTE=00
 PROCESS=32
 FILE="mobile_coupon.txt"
