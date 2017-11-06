939bdea579da55ca619b20b29373bbfc51e762f5
gogodick <geoffrey_lv@163.com>
2017-11-06 18:24:12 +0800

    Update activity

--- py/jd_sign.py
+++ py/jd_sign.py
@@ -317,7 +317,7 @@ class JDSign(JDWrapper):
                     sid = ck.value
                     break
             data = {
-                'activityno': '16-128171018023345181114',
+                'activityno': '16-139171031162017256643',
                 'sid': sid,
             }
             response = self.sess.get(sign_url, params=data)
