d60e5bfc1bce3553f1aed02cfcf774eec282b4f9
gogodick <geoffrey_lv@163.com>
2017-11-30 21:24:04 +0800

    update

--- coupon_mobile.sh
+++ coupon_mobile.sh
@@ -1,8 +1,8 @@
 URL=''
-URL='http://api.m.jd.com/client.action?functionId=newBabelAwardCollection&clientVersion=6.5.2&build=53662&client=android&androidId=e6f3d6adfea10bdd&installtionId=cd2474c771884269a103c12652b948d9&sdkVersion=23&uuid=357755070126840-e458b88e8e4c&st=1509660876593&sign=11e7dddb8bdb03fb43ec9ef95437dbe8&sv=100&body=%7B%22activityId%22%3A%22o1SAPkr8McqBZ8N1N9LhevAGKQg%22%2C%22scene%22%3A%221%22%2C%22args%22%3A%22key%3Debbf381a37944e3f92ede837445d7172%2CroleId%3D8754258%22%7D'
-KEY="ebbf381a37944e3f92ede837445d7172"
-ROLEID="8754258"
-HOUR=08
+URL='https://api.m.jd.com/client.action?functionId=newBabelAwardCollection&body=%7B%22activityId%22%3A%2232QepFHRksSYBrJX321BFdYQuhsX%22%2C%22from%22%3A%22H5node%22%2C%22scene%22%3A%221%22%2C%22args%22%3A%22key%3D659a8bd9e39f489094b9b36e4f4c27f9%2CroleId%3D9326973%22%2C%22mitemAddrId%22%3A%22%22%2C%22geo%22%3A%7B%22lng%22%3A%22%22%2C%22lat%22%3A%22%22%7D%7D&client=wh5&clientVersion=1.0.0'
+KEY="659a8bd9e39f489094b9b36e4f4c27f9"
+ROLEID="9326973"
+HOUR=22
 MINUTE=00
 PROCESS=32
 FILE="mobile_coupon.txt"
